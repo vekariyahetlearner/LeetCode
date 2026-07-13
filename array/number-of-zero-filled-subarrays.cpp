a8243ae0fdb2fb58a2d8a20169422dca658fca7c
vekariyahetlearner <vekariyahet.learner@gmail.com>
2026-07-13 11:15:44 +0530

    LC-number-of-zero-filled-subarrays [Runtime: N/A; Memory: N/A]

--- array/number-of-zero-filled-subarrays.cpp
+++ array/number-of-zero-filled-subarrays.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long count=0;
-        long long result=0;
-        for(int i=0;i<nums.size()-1;i++){
+        long long count=0, res=0;
+        for(int i=0;i<nums.size();i++){
             if(nums[i]==0){
                 count++;
-                if(nums[i]==0 && nums[i+1]!=0){
-                  result+=(count*(count+1))/2;
-                  count=0;
-                }
+            }
+            else{
+                res=res+(count*(count+1)/2);
+                count=0;
             }
         }
-        if(nums[nums.size()-1]==0) count++;
-        result+=(count*(count+1))/2;
-        return result;
+        res=res+(count*(count+1)/2);
+        return res;
     }
 };
