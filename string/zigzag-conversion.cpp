28352d4efcc604fe82d64b78511d8ed9c3845963
vekariyahetlearner <vekariyahet.learner@gmail.com>
2026-07-19 20:09:41 +0530

    LC-zigzag-conversion [Runtime: N/A; Memory: N/A]

--- string/zigzag-conversion.cpp
+++ string/zigzag-conversion.cpp
@@ -4,13 +4,13 @@ public:
         if (numRows == 1 || numRows >= s.size()) return s;
 
         string result;
-        int cycle = 2 * numRows - 2;
+        int cycle=2*numRows-2;
 
-        for (int row = 0; row < numRows; row++) {
-            for (int j = 0; j + row < s.size(); j += cycle) {
-                result += s[j + row];
-                if (row > 0 && row < numRows - 1 && j + cycle - row < s.size())
-                    result += s[j + cycle - row];
+        for (int row=0; row<numRows; row++) {
+            for (int j=0; j+row<s.size(); j+=cycle) {
+                result+=s[j+row];
+                if (row>0 && row<numRows-1 && j+cycle-row<s.size())
+                    result+=s[j+cycle-row];
             }
         }
         return result;
