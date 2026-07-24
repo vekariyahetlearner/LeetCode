f137f1c6f527681ce93349c11bb016067961b8ca
vekariyahetlearner <vekariyahet.learner@gmail.com>
2026-07-24 14:52:50 +0530

    LC-number-of-good-pairs [Runtime: 0 ms; Memory: 9.7 MB]

--- math/number-of-good-pairs.cpp
+++ math/number-of-good-pairs.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(auto i:nums) mp[i]++;
-        int count=0;
-        for(auto [k,v]:mp){
-            count+=((v)*(v-1))/2;
+        unordered_map<int, int> mp;
+        for(int num: nums){
+            mp[num]++;
+        }
+        int count = 0;
+        for (auto [k,v] : mp) {
+            count += v * (v - 1) / 2;
         }
         return count;
     }
